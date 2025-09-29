dcbb50aca272c9b1178bb662b3e96289f77e778f
Vaibhav More <vymore990@gmail.com>
2025-09-29 17:43:00 +0530

    secondcodechange

--- class_obj.dart
+++ class_obj.dart
@@ -1,10 +1,20 @@
 void main(){
 
    Person person = Person();
+   Person person1 = Person();
+
    person.name = "Vaibhav";
    person.surname = "More";
    person.age = 20;
+   person.isStudent = true;
+
+   person1.name = "John";
+   person1.surname = "Doe";
+   person1.age = 30;
+   person1.isStudent = false;
+
    person.displayInfo();
+   person1.displayInfo();
 
 }
 
@@ -12,11 +22,13 @@ class Person {
   String? name;
   String? surname;
   int? age;
+  bool? isStudent;
 
   void displayInfo() {
-    print("My name is ${name}");
-    print("My surname is ${surname}");
-    print("My age is ${age}");
+    print("My name is========> ${name}");
+    print("My surname is======> ${surname}");
+    print("My age is=========> ${age}");
+    print("isStudent========> ${isStudent}");
   }
 
 
